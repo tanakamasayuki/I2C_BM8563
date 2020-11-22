a36d38ed8b8f328e022762dd4bb70afb569b353c
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2020-11-22 12:00:32 +0100

    SetAlarmIRQ, clearIRQ, disableIRQ

--- src/I2C_BM8563.h
+++ src/I2C_BM8563.h
@@ -31,10 +31,20 @@ class I2C_BM8563 {
 
     void setTime(I2C_BM8563_TimeTypeDef* I2C_BM8563_TimeStruct);
     void setDate(I2C_BM8563_DateTypeDef* I2C_BM8563_DateStruct);
+  
+    int SetAlarmIRQ(int afterSeconds);
+    int SetAlarmIRQ( const I2C_BM8563_TimeTypeDef &I2C_BM8563_TimeStruct);
+    int SetAlarmIRQ( const I2C_BM8563_DateTypeDef &I2C_BM8563_DateStruct, const I2C_BM8563_TimeTypeDef &I2C_BM8563_TimeStruct);
+
+    void clearIRQ();
+    void disableIRQ();
 
   private:
     uint8_t bcd2ToByte(uint8_t value);
     uint8_t byteToBcd2(uint8_t value);
+  
+    void WriteReg(uint8_t reg, uint8_t data);
+    uint8_t ReadReg(uint8_t reg);
 
     uint8_t trdata[7];
     TwoWire *_i2cPort;
